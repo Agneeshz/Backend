647f0641352d81461e30d315e3e0bf226a66e586
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-07-09 05:21:35 +0530

    FU Cors

--- data.py
+++ data.py
@@ -1,114 +1,114 @@
 date = {
     '29': {
-        'Max':29,
-        'Min':23,
-        'Windspeed':4,
-        'Rainfall':8,
-        'Humidity':97
+        'Max': 29,
+        'Min': 23,
+        'Windspeed': 4,
+        'Rainfall': 8,
+        'Humidity': 97
     },
     '30': {
-        'Max':29,
-        'Min':23,
-        'Windspeed':6,
-        'Rainfall':2,
-        'Humidity':94
+        'Max': 29,
+        'Min': 23,
+        'Windspeed': 6,
+        'Rainfall': 2,
+        'Humidity': 94
     },
-    '1': {
-        'Max':31,
-        'Min':25,
-        'Windspeed':7,
-        'Rainfall':8,
-        'Humidity':89
+    '01': {
+        'Max': 31,
+        'Min': 25,
+        'Windspeed': 7,
+        'Rainfall': 8,
+        'Humidity': 89
     },
-    '2': {
-        'Max':33,
-        'Min':24,
-        'Windspeed':7,
-        'Rainfall':13,
-        'Humidity':84
+    '02': {
+        'Max': 33,
+        'Min': 24,
+        'Windspeed': 7,
+        'Rainfall': 13,
+        'Humidity': 84
     },
-    '3': {
-        'Max':34,
-        'Min':25,
-        'Windspeed':10,
-        'Rainfall':14,
-        'Humidity':80
+    '03': {
+        'Max': 34,
+        'Min': 25,
+        'Windspeed': 10,
+        'Rainfall': 14,
+        'Humidity': 80
     },
-    '4': {
-        'Max':34,
-        'Min':25,
-        'Windspeed':9,
-        'Rainfall':15,
-        'Humidity':83
+    '04': {
+        'Max': 34,
+        'Min': 25,
+        'Windspeed': 9,
+        'Rainfall': 15,
+        'Humidity': 83
     },
-    '5': {
-        'Max':36,
-        'Min':26,
-        'Windspeed':9,
-        'Rainfall':4,
-        'Humidity':74
+    '05': {
+        'Max': 36,
+        'Min': 26,
+        'Windspeed': 9,
+        'Rainfall': 4,
+        'Humidity': 74
     },
-    '6': {
-        'Max':35,
-        'Min':26,
-        'Windspeed':9,
-        'Rainfall':20,
-        'Humidity':81
+    '06': {
+        'Max': 35,
+        'Min': 26,
+        'Windspeed': 9,
+        'Rainfall': 20,
+        'Humidity': 81
     },
-    '7': {
-        'Max':34,
-        'Min':26,
-        'Windspeed':7,
-        'Rainfall':15,
-        'Humidity':81
+    '07': {
+        'Max': 34,
+        'Min': 26,
+        'Windspeed': 7,
+        'Rainfall': 15,
+        'Humidity': 81
     },
-    '8': {
-        'Max':33,
-        'Min':25,
-        'Windspeed':7,
-        'Rainfall':14,
-        'Humidity':84
+    '08': {
+        'Max': 33,
+        'Min': 25,
+        'Windspeed': 7,
+        'Rainfall': 14,
+        'Humidity': 84
     },
-    '9': {
-        'Max':33,
-        'Min':25,
-        'Windspeed':5,
-        'Rainfall':13,
-        'Humidity':84
+    '09': {
+        'Max': 33,
+        'Min': 25,
+        'Windspeed': 5,
+        'Rainfall': 13,
+        'Humidity': 84
     },
     '10': {
-        'Max':34,
-        'Min':26,
-        'Windspeed':4,
-        'Rainfall':7,
-        'Humidity':81
+        'Max': 34,
+        'Min': 26,
+        'Windspeed': 4,
+        'Rainfall': 7,
+        'Humidity': 81
     },
     '11': {
-        'Max':32,
-        'Min':26,
-        'Windspeed':4,
-        'Rainfall':12,
-        'Humidity':83
+        'Max': 32,
+        'Min': 26,
+        'Windspeed': 4,
+        'Rainfall': 12,
+        'Humidity': 83
     },
     '12': {
-        'Max':32,
-        'Min':25,
-        'Windspeed':7,
-        'Rainfall':12,
-        'Humidity':84
+        'Max': 32,
+        'Min': 25,
+        'Windspeed': 7,
+        'Rainfall': 12,
+        'Humidity': 84
     },
     '13': {
-        'Max':36,
-        'Min':26,
-        'Windspeed':6,
-        'Rainfall':0,
-        'Humidity':72
+        'Max': 36,
+        'Min': 26,
+        'Windspeed': 6,
+        'Rainfall': 0,
+        'Humidity': 72
     },
     '14': {
-        'Max':37,
-        'Min':27,
-        'Windspeed':5,
-        'Rainfall':1,
-        'Humidity':71
+        'Max': 37,
+        'Min': 27,
+        'Windspeed': 5,
+        'Rainfall': 1,
+        'Humidity': 71
     }
 }
